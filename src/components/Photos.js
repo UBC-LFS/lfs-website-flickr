30da26de2230fefb6a08f08bd7812e31ee3d13c3
Justin Lee <justinlee0022@gmail.com>
2017-02-16 16:14:02 -0800

    Images display

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Photos = (prop) => {
     console.log("the prop images are: ", prop);
+
+    const photos = prop.images.map((image, index) => {
+        if (image) {
+            const source = image;
+            return (
+                <img key={index} src={source} className="photoImage"/>
+            );
+        }
+    });
+
     return (
-        <div>I'm a photo component</div>
+      <div className="photoList">
+        {photos}
+      </div>
     );
 
 }
