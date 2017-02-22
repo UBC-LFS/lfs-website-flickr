f8d9735074570d23a09ca9fb2bdada1da18f364e
Justin Lee <justin.lee@ubc.ca>
2017-02-22 14:46:12 -0800

    Removed commented out code in slickslider.js

--- src/components/SlickSlider.js
+++ src/components/SlickSlider.js
@@ -5,22 +5,7 @@ const SimpleSlider = (prop) => {
   console.log(prop);
   // const SliderImage = prop.children.map( img => console.log(img.props.src))
   const images = prop.children.map( img => ({original: img.props.src, thumbnail: img.props.src}))
-
-  // const images = [
-  //     {
-  //       original: 'http://lorempixel.com/1000/600/nature/1/',
-  //       thumbnail: 'http://lorempixel.com/250/150/nature/1/',
-  //     },
-  //     {
-  //       original: 'http://lorempixel.com/1000/600/nature/2/',
-  //       thumbnail: 'http://lorempixel.com/250/150/nature/2/'
-  //     },
-  //     {
-  //       original: 'http://lorempixel.com/1000/600/nature/3/',
-  //       thumbnail: 'http://lorempixel.com/250/150/nature/3/'
-  //     }
-  //   ]
-    
+  
   return (
     <Slider items={images} slideInterval={2000}/>
   )
