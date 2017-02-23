9398f7dc8e69174a60b6312a67043ca9c978a161
Victor Wu <victorwu-95@hotmail.com>
2017-02-22 16:24:02 -0800

    UPDATED: slider with setstate error

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import API from './API/API';
 import Photos from './components/Photos';
 import SlickSlider from './components/SlickSlider'
+import $ from 'jquery';
 
 //const apiResult = API();
 
@@ -9,14 +10,50 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      photos: []
+      photos: [],
+      imageOption: '_h'
     }; 
   }
 
+  
+
+  onWindowResize(newSliderSize) {
+    // $(window).on('resize',function() {
+    //   console.log('asdf');
+    // });
+    let slider_container_size = $('#SliderContainer').width();
+    let container_option;
+    if (slider_container_size > 1600)
+    {
+      container_option = '_h';
+    }
+    else if (slider_container_size > 1024)
+    {
+      container_option = '_b';
+    }
+    else if (slider_container_size > 800)
+    {
+      container_option = '_c';
+    }
+    else
+    {
+      container_option = '_n';
+    }
+    this.setState({imageOption: '_b'});
+    console.log(container_option);
+  }
+
+// n, c, b, h
+  componentWillMount() {
+    this.onWindowResize();
+  }
   componentDidMount() {
-    API('_b').then(result => this.setState({'photos': result}))
+    $(window).on('resize', this.onWindowResize);
+    API('_h').then(result => this.setState({'photos': result}));
+    // window.addEventListener('resize', this.onWindowResize);
   }
   
+  // componentWillUnmount
   /*render() {
     return (
       <div>
@@ -27,7 +64,7 @@ export default class App extends React.Component {
   
   render() {
     return (
-      <div>
+      <div id="SliderContainer">
         <Photos images={this.state.photos}/>
       </div>
     )
