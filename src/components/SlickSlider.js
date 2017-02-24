3fad14790e3b1b36b59108e81078343806c926d0
Victor Wu <victorwu-95@hotmail.com>
2017-02-24 11:13:58 -0800

    FIXED: Lazy loading problem

--- src/components/SlickSlider.js
+++ src/components/SlickSlider.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Slider from 'react-image-gallery';
 
 const SimpleSlider = (prop) => {
-  //console.log(prop);
-  // const SliderImage = prop.children.map( img => console.log(img.props.src))
-  const images = prop.children.map( img => ({original: img.props.src, thumbnail: img.props.src}))
+
+  const images = prop.children.map( img => ({original: img.props.src, thumbnail: ''}))
   
   return (
-    <Slider items={images} slideInterval={2000}/>
-  )
+    <Slider items={images} lazyLoad={true} slideInterval={2000}/>
+  );
 }
 
 export default SimpleSlider;
