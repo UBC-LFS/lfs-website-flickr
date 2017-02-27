660a6e5fc4ed86a5a16319a0d65cbdfb7bb390c9
Justin Lee <justin.lee@ubc.ca>
2017-02-27 15:55:42 -0800

    Moved test API, changed error handling

--- src/API/API.js
+++ src/API/API.js
@@ -1,6 +1,5 @@
 const API_KEY = '74b6d18bf362b76dff862cf735bb0e1c';
 
-const TEST_API_CALL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=panda&extras=description&format=json&nojsoncallback=1`;
 const MFRE_IMAGES = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=72157677186721265&format=json&nojsoncallback=1`;
 
 
@@ -12,7 +11,7 @@ const fetchImages = (size) => {
               ({farm, server, id, secret}) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}${size}.jpg`
           );
       }).catch((error) => {
-          return "there's an error!";
+          return error;
       });
 };
 
