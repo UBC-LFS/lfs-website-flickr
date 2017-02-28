8b43bb6b6163235f8abd6593bd72c697cd19c22d
Justin Lee <justin.lee@ubc.ca>
2017-02-28 12:31:25 -0800

    Small styling changes

--- src/API/API.js
+++ src/API/API.js
@@ -2,21 +2,19 @@ const API_KEY = '74b6d18bf362b76dff862cf735bb0e1c';
 const MFRE_IMAGES = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=72157677186721265&format=json&nojsoncallback=1`;
 
 const fetchImages = (size) => {
-    let sizeString = '_b'; //default
+	let sizeString = '_b'; //default
 
-    (size >= 1800) ? sizeString = '_h' : 
-        (size >= 1000) ? sizeString = '_b' :  
-            sizeString = '_z'
+	(size >= 1800) ? sizeString = '_h' : (size >= 1000) ? sizeString = '_b' : sizeString = '_z'
 
-  return fetch(MFRE_IMAGES)
-      .then( response => response.json())
-      .then( json => {
-          return json.photoset.photo.map(
-              ({farm, server, id, secret}) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}${sizeString}.jpg`
-          );
-      }).catch((error) => {
-          return error;
-      });
+	return fetch(MFRE_IMAGES)
+		.then(response => response.json())
+		.then(json => {
+			return json.photoset.photo.map(
+				({ farm, server, id, secret}) => `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}${sizeString}.jpg`
+			);
+		}).catch((error) => {
+			return error;
+		});
 };
 
 export default fetchImages;
